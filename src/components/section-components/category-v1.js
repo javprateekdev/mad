ac178b6bbb1afc24834cb958731c7f98ad6f3779
javprateekdev <113234677+javprateekdev@users.noreply.github.com>
2022-12-09 16:54:38 +0530

    Update category-v1.js

--- src/components/section-components/category-v1.js
+++ src/components/section-components/category-v1.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import parse from 'html-react-parser';
 
 class CategoryV1 extends Component {
 
@@ -22,74 +22,74 @@ class CategoryV1 extends Component {
 			    <div className="row ltn__category-slider-active--- slick-arrow-1 justify-content-center">
 			      <div className="col-lg-3 col-md-4 col-sm-6 col-6">
 			        <div className="ltn__category-item ltn__category-item-5 text-center">
-			          
+			          <Link>
 			            <span className="category-icon"><i className="flaticon-car" /></span>
 			            <span className="category-title">Parking Space</span>
 			            <span className="category-btn"><i className="flaticon-right-arrow" /></span>
-			          
+			          </Link>
 			        </div>
 			      </div>
 			      <div className="col-lg-3 col-md-4 col-sm-6 col-6">
 			        <div className="ltn__category-item ltn__category-item-5 text-center">
-			          
+			          <Link >
 			            <span className="category-icon"><i className="flaticon-swimming" /></span>
 			            <span className="category-title">Swimming Pool</span>
 			            <span className="category-btn"><i className="flaticon-right-arrow" /></span>
-			         
+			          </Link>
 			        </div>
 			      </div>
 			      <div className="col-lg-3 col-md-4 col-sm-6 col-6">
 			        <div className="ltn__category-item ltn__category-item-5 text-center">
-			          
+			          <Link >
 			            <span className="category-icon"><i className="flaticon-secure-shield" /></span>
 			            <span className="category-title">Private Security</span>
 			            <span className="category-btn"><i className="flaticon-right-arrow" /></span>
-			         
+			          </Link>
 			        </div>
 			      </div>
 			      <div className="col-lg-3 col-md-4 col-sm-6 col-6">
 			        <div className="ltn__category-item ltn__category-item-5 text-center">
-			         
+			          <Link >
 			            <span className="category-icon"><i className="flaticon-stethoscope" /></span>
 			            <span className="category-title">Medical Center</span>
 			            <span className="category-btn"><i className="flaticon-right-arrow" /></span>
-			        
+			          </Link>
 			        </div>
 			      </div>
 			      <div className="col-lg-3 col-md-4 col-sm-6 col-6">
 			        <div className="ltn__category-item ltn__category-item-5 text-center">
-			         
+			          <Link >
 			            <span className="category-icon"><i className="flaticon-book" /></span>
 			            <span className="category-title">Library Area</span>
 			            <span className="category-btn"><i className="flaticon-right-arrow" /></span>
-			          
+			          </Link>
 			        </div>
 			      </div>
 			      <div className="col-lg-3 col-md-4 col-sm-6 col-6">
 			        <div className="ltn__category-item ltn__category-item-5 text-center">
-			         
+			          <Link >
 			            <span className="category-icon"><i className="flaticon-bed-1" /></span>
 			            <span className="category-title">King Size Beds</span>
 			            <span className="category-btn"><i className="flaticon-right-arrow" /></span>
-			       
+			          </Link>
 			        </div>
 			      </div>
 			      <div className="col-lg-3 col-md-4 col-sm-6 col-6">
 			        <div className="ltn__category-item ltn__category-item-5 text-center">
-			          
+			          <Link >
 			            <span className="category-icon"><i className="flaticon-home-2" /></span>
 			            <span className="category-title">Smart Homes</span>
 			            <span className="category-btn"><i className="flaticon-right-arrow" /></span>
-			         
+			          </Link>
 			        </div>
 			      </div>
 			      <div className="col-lg-3 col-md-4 col-sm-6 col-6">
 			        <div className="ltn__category-item ltn__category-item-5 text-center">
-			         
+			          <Link>
 			            <span className="category-icon"><i className="flaticon-slider" /></span>
 			            <span className="category-title">Kid’s Playland</span>
 			            <span className="category-btn"><i className="flaticon-right-arrow" /></span>
-			         
+			          </Link>
 			        </div>
 			      </div>
 			    </div>
